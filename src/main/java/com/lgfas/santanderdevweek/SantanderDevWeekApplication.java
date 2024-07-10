fc115a163378d1a20d1389371d78d7bfbf2625fc
lgfas <luis.guilherme2811@gmail.com>
2024-07-10 11:12:43 -0300

    Bugfix CORS

--- src/main/java/com/lgfas/santanderdevweek/SantanderDevWeekApplication.java
+++ src/main/java/com/lgfas/santanderdevweek/SantanderDevWeekApplication.java
@@ -1,11 +1,13 @@
 package com.lgfas.santanderdevweek;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class SantanderDevWeekApplication {
 
